6af84b41b078feabdd4f8c38f2a3d548c05bdb55
Valera <40528690+Valerkaa@users.noreply.github.com>
2023-11-12 08:38:23 -0800

    Revert "lab2"

--- IManageable.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface IManageable
-{
-    public void addItem(Item akItem);
-    public void removeItem(Item akItem);
-    public List<Item> listAvailable();
-    public List<Item> listBorrowed();
-}
